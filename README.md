e7b7618b13c6add3a68f3d969252f13c8d040be2
Satoshi Kobayashi <koba@yk.rim.or.jp>
2014-10-07 13:49:13 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Foo::Bar - It's new $module
+
+# SYNOPSIS
+
+    use Foo::Bar;
+
+# DESCRIPTION
+
+Foo::Bar is ...
+
+# LICENSE
+
+Copyright (C) Satoshi Kobayashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Satoshi Kobayashi <koba@yk.rim.or.jp>
---
+++ lib/Foo/Bar.pm
@@ -0,0 +1,39 @@
+package Foo::Bar;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Foo::Bar - It's new $module
+
+=head1 SYNOPSIS
+
+    use Foo::Bar;
+
+=head1 DESCRIPTION
+
+Foo::Bar is ...
+
+=head1 LICENSE
+
+Copyright (C) Satoshi Kobayashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Satoshi Kobayashi E<lt>koba@yk.rim.or.jpE<gt>
+
+=cut
+
